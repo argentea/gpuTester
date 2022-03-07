3af8cfff2c8760415a0b259317a7d3275574363f
kunpengjiang <argentea@pku.edu.cn>
2022-03-07 20:37:21 +0800

    Add ptr ask function

--- flatArray/database/database.cpp
+++ flatArray/database/database.cpp
@@ -5,5 +5,12 @@ namespace FLATARRAY {
 FlatArray::FlatArray(int size): size(size) {
 }
 
+tValue* FlatArray::getDevicePtr() {
+    return dv_ptr;
+}
+
+tValue* FlatArray::getHostPtr() {
+    return hv_ptr;
+}
 
 };
