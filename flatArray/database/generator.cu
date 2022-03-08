6c0b2bd008926ad3d365bf122fda8662b86af78f
kunpengjiang <argentea@pku.edu.cn>
2022-03-08 16:34:33 +0800

    Add segGenerator

---
+++ flatArray/database/generator.cu
@@ -0,0 +1,21 @@
+#include "generator.h"
+
+__global__ void __launch_bounds__(1024)
+incSegGen(tValue* dv_ptr, tSize size, tSize step){
+	const int threadId = threadIdx.x;
+	const int threadNum = blockDim.x;
+	const int blockId = blockIdx.x;
+	const int blockNum = gridDim.x;
+
+	int tsize = step;
+	int bucket_num = size/step;
+	for(int i = blockId; i < bucket_num; i += blockNum){
+		if(step * (i + 1) > size){
+			tsize = size - step*i;
+		}
+		for(int j = threadId; j < tsize; j += threadNum){
+			dv_ptr[i*size + j] = i;
+		}
+	}
+	__syncthreads();
+}
---
+++ flatArray/database/generator.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <cuda_runtime.h>
+#include <cuda_profiler_api.h>
+
+#include "database.h"
+#include "utils.h"
+
+using namespace FLATARRAY;
+__global__ void incSegGen(tValue* dv_ptr, tSize size, tSize step);
