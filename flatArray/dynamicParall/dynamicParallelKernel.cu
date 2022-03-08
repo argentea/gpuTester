49b6d85a1621d51ffec30b50e35adecf8b726b8c
kunpengjiang <argentea@pku.edu.cn>
2022-03-08 16:07:55 +0800

    Create kernel and prepare for generate data

--- flatArray/dynamicParall/dynamicParallelKernel.cu
+++ flatArray/dynamicParall/dynamicParallelKernel.cu
@@ -10,7 +10,44 @@ void FaDPSolver::solve(){
 
 }
 
-__global__ void dynamicParallelKernel(tValue* dv_ptr, tSize size, tSize step_size){
+//step size shouldn't be too small. One child kernel process step size values.
+__global__ void __launch_bounds__(32)
+dynamicParallelKernel(tValue* dv_ptr, tSize size, tSize step_size){
+	const int threadId = threadIdx.x;
+	const int threadNum = blockDim.x;
+
+	tValue* dst_ptr;
+	tValue* src_ptr;
+	tSize child_size = 0;
+	
+	int num_launch1 = size/(2*step_size);
+
+	for(int i = threadId; i < num_launch1; i += threadNum){
+		dst_ptr = dv_ptr + i*step_size;
+		src_ptr = dv_ptr + step_size;
+		if((2*i + 1) * step_size > size){
+			child_size = size - 2*i*step_size;
+		}else {
+			child_size = step_size;
+		}
+
+		dynamicParallelKernelChild<<<1, 512>>>(dst_ptr, src_ptr, child_size);
+	}
+	__syncthreads();
+
+	int num_launch2 = (size - step_size)/(2*step_size);
+
+	for(int i = threadId; i < num_launch1; i += threadNum){
+		dst_ptr = dv_ptr + step_size + i*step_size;
+		src_ptr = dv_ptr + step_size;
+		if((2*i + 1) * step_size > size - step_size){
+			child_size = size - step_size - 2*i*step_size;
+		}else {
+			child_size = step_size;
+		}
+		dynamicParallelKernelChild<<<1, 512>>>(dst_ptr, src_ptr, child_size);
+	}
+	__syncthreads();
 
 }
 
@@ -22,6 +59,7 @@ dynamicParallelKernelChild(tValue* dst_ptr, tValue* src_ptr, tSize size){
 	for(int i = threadId; i < size; i+= threadNum){
 		dst_ptr[i] += src_ptr[i];
 	}
+	__syncthreads();
 	return;
 }
 
