291c0c41548789f5b0dad0c9e9805e35b884ea5c
kunpengjiang <argentea@pku.edu.cn>
2022-03-08 16:37:08 +0800

    Change test.cpp to test solve
    Now, we have to add time macro;

--- flatArray/test.cpp
+++ flatArray/test.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
 #include "database.h"
+#include "dynamicParallelKernel.h"
 
 
 using namespace std;
 
 int main(){
 	using namespace FLATARRAY;
+	using namespace FADPKERNEL;
 	cout << "This is flatArray test\n";
 
 	int testSize = 1024*1024*1024;
 
 	FlatArray fatest;
 	fatest.init(testSize);
+
+	FaDPSolver solver(fatest.getDevicePtr(), fatest.getSize());
+	solver.solve();
 	return 0;
 }
